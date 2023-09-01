0ef095dcff50f87204af1a93f784ca102305c8e4
97ahmadshah <97ahmadshah@gmail.com>
2023-09-01 13:55:44 -0700

    Fixing seeders

--- backend/db/seeders/20230831232331-demo-user.js
+++ backend/db/seeders/20230831232331-demo-user.js
@@ -14,17 +14,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: "ahmad",
+        lastName: 'shah'
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: "ahmad",
+        lastName: 'shah'
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: "ahmad",
+        lastName: 'shah'
       }
     ], { validate: true });
   },
